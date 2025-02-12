c22c855ac9b2d1d7e70dca00eef3c3bd2a3bd4a8
wandi1209 <wandider@gmail.com>
2025-02-12 18:34:08 +0700

    add font poppins for project

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: "Poppins"),
       home: Scaffold(
         body: Center(child: Text("Firebase Working")),
       ),
