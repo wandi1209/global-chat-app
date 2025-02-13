f4adc51783a2559cb5252c0c1a252aa9822b8caa
wandi1209 <wandider@gmail.com>
2025-02-13 21:44:55 +0700

    create user login and splash forwarding

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:globalchat/screens/dashboard_screen.dart';
+
+class LoginController {
+  static Future<void> login(
+      {required BuildContext context,
+      required String email,
+      required String password}) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute(builder: (context) {
+        return DashboardScreen();
+      }), (route) {
+        return false;
+      });
+    } catch (e) {
+      SnackBar messageSnackBar =
+          SnackBar(backgroundColor: Colors.red, content: Text(e.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(messageSnackBar);
+    }
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:globalchat/controllers/login_controller.dart';
+import 'package:globalchat/controllers/signup_controller.dart';
+import 'package:globalchat/screens/dashboard_screen.dart';
 import 'package:globalchat/screens/signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,23 +13,86 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var userForm = GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Column(
-        children: [
-          Text("Don't have an account?"),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return SignupScreen();
-                }));
-              },
-              child: Text("Sign up now!"))
-        ],
+      body: Form(
+        key: userForm,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: email,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Email is required";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  label: Text("Email"),
+                ),
+              ),
+              SizedBox(height: 23),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: password,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Password is required";
+                  }
+                  return null;
+                },
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  label: Text("Password"),
+                ),
+              ),
+              SizedBox(height: 23),
+              ElevatedButton(
+                  onPressed: () {
+                    if (userForm.currentState!.validate()) {
+                      LoginController.login(
+                          context: context,
+                          email: email.text,
+                          password: password.text);
+                    }
+                  },
+                  child: Text("Login")),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text("Don't have account?"),
+                  SizedBox(width: 5),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return SignupScreen();
+                      }));
+                    },
+                    child: Text(
+                      "Sign up here!",
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
